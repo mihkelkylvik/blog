bff9f53276414078d4faf3dcdbdc656784330d7b
Mihkel <mihkel.kulvik@khk.ee>
2014-09-04 16:51:52 +0300

    Customize project settings

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -3,7 +3,7 @@
         <h1><?=$post['post_subject']?></h1>
         <p><?=$post['post_text']?></p>
         <div>
-            <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+            <span class="badge badge-success">P0sted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
         </div>
         <hr>
     </div>
