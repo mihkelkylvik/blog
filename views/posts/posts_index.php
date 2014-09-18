6a0c866ea82e0497259ab3209503d98aa6ee5e5a
Mihkel <mihkel.kulvik@khk.ee>
2014-09-19 00:51:29 +0300

    Customize project settings

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -3,8 +3,14 @@
         <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject']?></a></h1>
         <p><?=$post['post_text']?></p>
         <div>
-            <span class="badge badge-success">P0sted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+            <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
         </div>
         <hr>
     </div>
 <?endforeach?>
+<hr>
+<form method='post'>
+    <input type="text" placeholder="Pealkiri"/><br/><br />
+    <textarea name ='data[post_text]' cols="30" rows="20" placeholder="Sisu"></textarea> <br/><br/>
+    <botton class="btn">Post</botton>
+</form>
