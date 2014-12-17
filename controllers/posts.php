2c2412684732fb8761561bd4cb4869ec75d90ec9
Mihkel <Anonymous@gmail.com>
2014-12-17 11:59:57 +0200

    Syntax fix

--- controllers/posts.php
+++ controllers/posts.php
@@ -3,7 +3,7 @@
 class posts extends Controller{
 
 	function index(){
-		$this->posts = get_all("SELECT * FROM posts");
+		$this->posts = get_all("SELECT * FROM post");
         $_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
         foreach ($_tags as $tag){
             $this->tags[$tag['post_id']][] = $tag['tag_name'];
